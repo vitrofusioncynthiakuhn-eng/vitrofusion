63fdabad1bb75ab057787119a0e511fb8517cbaf
melany909 <melanysolhlum@gmail.com>
2026-07-11 20:08:35 -0300

    probando solucionar errores

--- script.js
+++ script.js
@@ -12,8 +12,6 @@ const catalogo = document.querySelector("#catalogo");                   //Guarda
 ============ (muestra el catálogo y oculta el inicio) =========*/
 botonCatalogo.addEventListener("click", function () {
 
-    alert("Catálogo abierto");
-
     encabezado.classList.add("oculto");
     presentacion.classList.add("oculto");
     catalogo.classList.remove("oculto");
