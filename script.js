30dfd3e60315a2460356fed43abedade118efe04
melany909 <melanysolhlum@gmail.com>
2026-07-13 19:37:12 -0300

    actualizaciones

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const botonVolver = document.querySelector("[data-cerrar-catalogo]");         /*
 const encabezado = document.querySelector(".encabezado");                    /*Guarda todo el hero*/
 const presentacion = document.querySelector(".presentacion");            /*Guarda la parte que dice: trabajo personalizado.....*/
 const catalogo = document.querySelector("#catalogo");   
+const botonContacto = document.querySelector("[data-abrir-contacto]");
+const botonVolverContacto = document.querySelector("[data-cerrar-contacto]");
+const contacto = document.querySelector("#contacto");
 
 /* ================= ELEMENTOS DEL MODAL ================= */
 const modalProducto = document.querySelector("#modal-producto");
@@ -32,8 +35,22 @@ const tarjetas = document.querySelectorAll(".tarjeta-producto");//Guarda toda la
 botonCatalogo.addEventListener("click", function () {
 
     encabezado.classList.add("oculto");
+
     presentacion.classList.add("oculto");
+
     catalogo.classList.remove("oculto");
+
+    tarjetas.forEach(function(tarjeta, indice){
+
+        tarjeta.classList.remove("aparecer");
+
+        setTimeout(function(){
+
+            tarjeta.classList.add("aparecer");
+
+        }, indice * 120);
+
+    });
 
 });
 
@@ -41,6 +58,38 @@ botonCatalogo.addEventListener("click", function () {
 botonVolver.addEventListener("click", function () {
 
     catalogo.classList.add("oculto");
+
+    encabezado.classList.remove("oculto");
+
+    presentacion.classList.remove("oculto");
+
+});
+
+/* ===================== ABRIR CONTACTO ===================== */
+botonContacto.addEventListener("click", function () {
+
+    encabezado.classList.add("oculto");
+
+    presentacion.classList.add("oculto");
+
+    catalogo.classList.add("oculto");
+
+    contacto.classList.remove("oculto");
+
+    setTimeout(function(){
+
+        contacto.classList.add("mostrar");
+
+    },20);
+
+});
+
+/* ===================== VOLVER AL INICIO DESDE CONTACTO ===================== */
+botonVolverContacto.addEventListener("click", function () {
+
+    contacto.classList.remove("mostrar");
+
+    contacto.classList.add("oculto");
 
     encabezado.classList.remove("oculto");
 
@@ -85,3 +134,29 @@ botonCerrarModal.addEventListener("click", function(){
     modalProducto.classList.add("oculto");
 
 });
+
+/* =================== SLIDER DEL ENCABEZADO =================== */
+
+const slides = document.querySelectorAll(".slide");     /*Busca todas las imág. que tienen la clase .slide (las imagenes en el html), las guarda todas juntas en una lista*/
+
+let indiceSlide = 0;          /*Es una variable, guarda cual imág. está mostrando (imagen 1, ....)*/
+
+setInterval(function () {           /*Muy usada, "repetí este código cada cierto tiempo", el tiempo esta al final, en este caso es 400(4seg)*/
+
+    slides[indiceSlide].classList.remove("activo");             
+
+    indiceSlide++;
+
+    if (indiceSlide >= slides.length) {             /*Es tipo, llegaste al final de todas las imág.? bueno, volve al principio*/
+
+        indiceSlide = 0;                      /*Ahí vuelve a empezar*/
+
+    }
+
+    slides[indiceSlide].classList.add("activo");
+
+}, 4000);
+
+
+
+
